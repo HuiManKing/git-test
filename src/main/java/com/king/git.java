cd9e4947ee9f7c1ede3aab47c6fe95251f3641ce
HuiManKing <88771396+HuiManKing@users.noreply.github.com>
2022-04-09 10:32:43 +0800

    GitHub commit

--- src/main/java/com/king/git.java
+++ src/main/java/com/king/git.java
@@ -3,6 +3,6 @@ package com.king;
 public class git {
     public static void main(String[] args) {
         System.out.println("Hello Git Push");
-
+        System.out.println("Hello GitHub")
     }
 }
