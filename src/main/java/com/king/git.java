486eb50b698bade7d2e8e21ab5cd14045cacf651
HuiKing <1127451746@qq.com>
2022-04-09 09:35:00 +0800

    master commit 2

--- src/main/java/com/king/git.java
+++ src/main/java/com/king/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("Hello Hot-fix");
+        System.out.println("master test");
     }
 }
